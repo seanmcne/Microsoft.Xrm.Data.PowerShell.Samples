49810365c6b788bd0196c996d044c5e848027671
Thom Schumacher <crshnbrn66@gmail.com>
2017-04-11 14:59:20 -0700

    Update AddCrmUsersFromCSV.ps1

--- Microsoft.Xrm.Data.PowerShell.Samples/AddCrmUsersFromCSV/AddCrmUsersFromCSV.ps1
+++ Microsoft.Xrm.Data.PowerShell.Samples/AddCrmUsersFromCSV/AddCrmUsersFromCSV.ps1
@@ -14,8 +14,8 @@
 # and (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys’ fees, that arise or result from the use or distribution of the Sample Code 
 param
 (
-  [string]$ServerUrl = 'http://crm.dev.pultegroup.com:80',
-  [string]$OrganizationName = 'PulteGroup',
+  [string]$ServerUrl = 'http://yourServer:80',
+  [string]$OrganizationName = 'YourOrg',
   [Parameter(Mandatory = $true)]
   [pscredential]$AdminUserCredentials = (Get-Credential),
   [string]$csvPath = ".\ADUsers.csv"
