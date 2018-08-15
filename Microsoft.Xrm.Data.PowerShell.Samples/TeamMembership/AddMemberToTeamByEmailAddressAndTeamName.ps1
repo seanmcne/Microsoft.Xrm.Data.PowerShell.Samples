3beebfbf6878572cd38164505e214d6e79b683b0
Sean McNellis <seanmcne@hotmail.com>
2018-08-15 10:51:49 -0500

    Update AddMemberToTeamByEmailAddressAndTeamName.ps1

--- Microsoft.Xrm.Data.PowerShell.Samples/TeamMembership/AddMemberToTeamByEmailAddressAndTeamName.ps1
+++ Microsoft.Xrm.Data.PowerShell.Samples/TeamMembership/AddMemberToTeamByEmailAddressAndTeamName.ps1
@@ -1,4 +1,3 @@
-
 function addUserToTeam{
     [CmdletBinding()]
     param(
@@ -7,12 +6,15 @@ function addUserToTeam{
         [parameter(Mandatory=$true)]
         [string]$teamName
     )
+    
     $teams = get-crmrecords -EntityLogicalName team -Fields name -FilterAttribute name -FilterOperator like -FilterValue $teamName #
+
     if($teams.Count -gt 1){
         throw "Error - more than one team returned for: $teamName"
     }else{
         Write-Output "Found team $teamName with Id: $($teams.CrmRecords[0].teamid)"
     }
+
     $fetch=
 @"
         <fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false' no-lock='true'>
@@ -25,18 +27,30 @@ function addUserToTeam{
                 </entity>
             </fetch>
 "@
+
     $users = Get-CrmRecordsByFetch $fetch -Verbose
+    
     if($users.Count -gt 1){
         throw "Error - more than one user was returned for: $userUpn"
     }else{
         Write-Output "Found user $userUpn with Id: $($users.CrmRecords[0].systemuserid)"
     }
-    if($teams.Count -eq 0 -and $users.Count -eq 0){
+
+    if($teams.Count -eq 1 -and $users.Count -eq 1){
+         #ID of the team list you want to add members (or a single member) to 
+
         $teamMembers = @(); 
+
         $teamMembers += @($users.CrmRecords[0].systemuserid)
+
+        #$teamMembers += @("E563BB21-E3E4-A1CF-40A5-C8975E9C3860" )
+
         $AddMember = new-object Microsoft.Crm.Sdk.Messages.AddMembersTeamRequest
+
         $AddMember.TeamId = $teams.CrmRecords[0].teamid
+
         $AddMember.MemberIds = $teamMembers 
+
         $conn.ExecuteCrmOrganizationRequest($AddMember)
         if($conn.LastCrmException -ne $null){
             throw $conn.LastCrmException
@@ -44,8 +58,9 @@ function addUserToTeam{
         if($conn.LastCrmError -ne $null){
                     throw $conn.LastCrmError
         }
+
     }
     else {
-        return;
+        throw "Add Member To Team was not completed"
     }
 }
