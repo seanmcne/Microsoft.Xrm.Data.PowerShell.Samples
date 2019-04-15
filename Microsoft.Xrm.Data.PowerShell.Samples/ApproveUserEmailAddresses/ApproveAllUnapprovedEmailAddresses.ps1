41a07955c9952c7ff87d5dbe86316a66378cf61b
Sean McNellis <seanmcne@hotmail.com>
2019-04-15 16:57:00 -0500

    Update ApproveAllUnapprovedEmailAddresses.ps1
    added test & enable operation

--- Microsoft.Xrm.Data.PowerShell.Samples/ApproveUserEmailAddresses/ApproveAllUnapprovedEmailAddresses.ps1
+++ Microsoft.Xrm.Data.PowerShell.Samples/ApproveUserEmailAddresses/ApproveAllUnapprovedEmailAddresses.ps1
@@ -5,8 +5,7 @@
 Import-module Microsoft.xrm.data.powershell
 
 #you can connect via the UI, for Azure Automation or scripted runs, use get-crmconnection instead 
-
-Connect-CrmOnlineDiscovery -InteractiveMode
+Connect-CrmOnline -ServerUrl "instance.crm.dynamics.com" -ForceOAuth 
 
 #the following are the values for emailrouteraccessapproval 
 #0 Empty; 1 Approved; 2 PendingApproval; 3 Rejected 
@@ -14,8 +13,12 @@ Connect-CrmOnlineDiscovery -InteractiveMode
 #get all users that do not have an approved status
 $unapprovedUsers = Get-CrmRecords -EntityLogicalName systemuser -FilterAttribute emailrouteraccessapproval -FilterOperator neq -FilterValue 1
 
+#approve all unapproved users - then schedule them for a test&enable
 $unapprovedUsers.CrmRecords | 
 foreach{
   Write-Output "Approving $($_.systemuserid)"; 
   Approve-CrmEmailAddress -UserId $_.systemuserid;
+  
+  #test & enable mailbox, reset org flag for primary sync org, and schedule test for enabling: 
+  Set-CrmUserMailbox -UserId $_.systemuserid -MarkedAsPrimaryForExchangeSync $true -ApplyDefaultEmailSettings -ScheduleTest
 }
