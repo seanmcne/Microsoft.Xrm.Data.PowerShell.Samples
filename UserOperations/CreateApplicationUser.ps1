94c470c334c563df9ddc3247405a200c67b7cae8
Sean McNellis <seanmcne@hotmail.com>
2020-06-25 15:00:12 -0500

    Update CreateApplicationUser.ps1

--- UserOperations/CreateApplicationUser.ps1
+++ UserOperations/CreateApplicationUser.ps1
@@ -3,5 +3,5 @@ New-CrmRecord -EntityLogicalName systemuser -Fields @{
   "lastname"="App";
   "internalemailaddress"="appuser@domain.com";
   "applicationid"=[Guid]"cb94b033-d8d2-4bd3-a5fd-0def181145e2";
-  "businessunitid"=(New-CrmEntityReference -Id (Invoke-CrmWhoAmI).BusinessUnitId)
+  "businessunitid"=(New-CrmEntityReference -Id (Invoke-CrmWhoAmI).BusinessUnitId -EntityLogicalName systemuser)
 }
